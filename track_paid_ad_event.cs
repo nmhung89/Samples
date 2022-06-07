d56ae51ef1a15d070f97ed7dd4705578798ebbf9
Minh-Hung Nguyen <nmhung89@gmail.com>
2022-06-07 22:31:06 +1000

    Create track_paid_ad_event.cs

---
+++ track_paid_ad_event.cs
@@ -0,0 +1,59 @@
+protected override void RequestInterstitialAds()
+{
+    if (mInterstitialData == null)
+    {
+        mInterstitialData = new InterstitialAd(InterstitialId);
+        
+        mInterstitialData.OnPaidEvent += (sender, args) =>
+        {
+            var interstitialAd = (InterstitialAd) (sender);
+            if (interstitialAd != null)
+            {
+                var info = interstitialAd.GetResponseInfo();
+                trackPaidAdEvent(args, Config.Platform.InterstitialId, info.GetMediationAdapterClassName());
+            }
+            else
+            {
+                trackPaidAdEvent(args, Config.Platform.InterstitialId, null);
+            }  
+        };
+        
+    }
+
+    AdRequest request = new AdRequest.Builder().Build();
+    mInterstitialData.LoadAd(request);
+}
+
+protected override void RequestRewardedVideoAds() {
+        // ........
+        // similar to interstitial
+        mRewardedVideoData.OnPaidEvent += (sender, args) =>
+        {
+        }
+        // ........
+}
+
+protected override void RequestBannerAds() {
+        // ........
+        // similar to interstitial
+        mBannerData.OnPaidEvent += (sender, args) =>
+        {
+        }
+        // ........
+}
+
+ public static void trackPaidAdEvent( AdValueEventArgs args, string adUnit, string adNetworkName) {
+     Dictionary<string, object> Dic = new Dictionary<string, object>();
+     Dic["valuemicros"] = args.AdValue.Value;
+     Dic["currency"] = args.AdValue.CurrencyCode;
+     Dic["precision"] = args.AdValue.Precision;
+     Dic["adunitid"] = adUnit;
+
+     if (adNetworkName != null)
+     {
+         Dic["network"] = adNetworkName;
+     }
+     Debug.Log($"trackPaidAdEvent  \n valuemicros {args.AdValue.Value} \n currency {args.AdValue.CurrencyCode} \n precision {args.AdValue.Precision} ");
+     
+     Firebase.Analytics.FirebaseAnalytics.LogEvent("admob_ad_revenue", Dic);
+ }
